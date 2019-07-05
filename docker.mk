98a0c0d4483e4a17d12f76fb347cae536861e1b0
Wil Roboly <wilco@nurf.com>
2019-07-05 12:57:28 -0400

    Initial commit

---
+++ docker.mk
@@ -0,0 +1,62 @@
+include .env
+
+default: up
+
+## help	:	Print commands help.
+.PHONY: help
+ifneq (,$(wildcard docker.mk))
+help : docker.mk
+	@sed -n 's/^##//p' $<
+else
+help : Makefile
+	@sed -n 's/^##//p' $<
+endif
+
+## up	:	Start up containers.
+.PHONY: up
+up:
+	@echo "Starting up containers for $(PROJECT_NAME)..."
+	docker-compose -f traefik.yml pull
+	docker-compose -f traefik.yml up -d --remove-orphans
+
+## down	:	Stop containers.
+.PHONY: down
+down: stop
+
+## start	:	Start containers without updating.
+.PHONY: start
+start:
+	@echo "Starting containers for $(PROJECT_NAME) from where you left off..."
+	@docker-compose -f traefik.yml start
+
+## stop	:	Stop containers.
+.PHONY: stop
+stop:
+	@echo "Stopping containers for $(PROJECT_NAME)..."
+	@docker-compose -f traefik.yml stop
+
+## prune	:	Remove containers and their volumes.
+##		You can optionally pass an argument with the service name to prune single container
+##		prune mariadb	: Prune `mariadb` container and remove its volumes.
+##		prune mariadb solr	: Prune `mariadb` and `solr` containers and remove their volumes.
+.PHONY: prune
+prune:
+	@echo "Removing containers for $(PROJECT_NAME)..."
+	@docker-compose -f traefik.yml down -v $(filter-out $@,$(MAKECMDGOALS))
+
+## ps	:	List running containers.
+.PHONY: ps
+ps:
+	@docker ps --filter name='$(PROJECT_NAME)*'
+
+## logs	:	View containers logs.
+##		You can optinally pass an argument with the service name to limit logs
+##		logs php	: View `php` container logs.
+##		logs nginx php	: View `nginx` and `php` containers logs.
+.PHONY: logs
+logs:
+	@docker-compose -f traefik.yml logs -f $(filter-out $@,$(MAKECMDGOALS))
+
+# https://stackoverflow.com/a/6273809/1826109
+%:
+	@:
